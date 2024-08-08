b6cd7604dd6a85b20cd0550c4a836b6fba7c0bce
codedreammer <nkanal38@gmail.com>
2024-08-08 16:26:25 +0530

    update the code

--- constructor.py
+++ constructor.py
@@ -1,14 +1,16 @@
 class Person:
-    def __init__(self, n,o):
-        print("Hey I am a person")
+    def __init__(self, n,o,p):
         self.name = n
         self.occ = o
+        self.phone = p
 
 
     def info(self):
-        print(f"{self.name} is a {self.occ}")
+        print(f"{self.name} is a {self.occ} their phone {self.phone}")
 
-a = Person("akshay","web dev")
-b = Person("diviya", "worker")
+a = Person("akshay","web dev","MI")
+b = Person("diviya", "worker","oppo")
+c = Person("alok","writer","iphone")
 a.info()
 b.info()
+c.info()
