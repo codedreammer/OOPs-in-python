b6c6c6087534f8d92937eb0d27660b4e025f97f3
codedreammer <nkanal38@gmail.com>
2024-08-10 14:35:14 +0530

    updated decorators

--- Decorators.py
+++ Decorators.py
@@ -22,16 +22,17 @@ add(3, 4)
 import logging
 
 def log_function_call(fun):
-    def decorated(*args,**kwargs):
-        logging.info(f"calling {func.__name__} with args={args}, kwargs={kwargs}")
+    def decorated(*args, **kwargs):
+        logging.info(f"calling {fun.__name__} with args={args}, kwargs={kwargs}")
         result = fun(*args, **kwargs)
-        logging.info(f"{func.__name__} returned {result}")
+        logging.info(f"{fun.__name__} returned {result}")
         return result
     return decorated
 
 @log_function_call
 def my_function(a, b):
-    return a+b
+    return a + b
 
-
-
+# Example usage
+logging.basicConfig(level=logging.INFO)
+my_function(3, 4)
